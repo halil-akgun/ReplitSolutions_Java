7490c9b30fdbb140525f7935b5985a135d93431e
halil <halilakgun0@gmail.com>
2023-01-11 21:22:08 +0300

    R07_ENG_For_while_6

--- src/R07_ENG_For_while_6.java
+++ src/R07_ENG_For_while_6.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class R07_ENG_For_while_6 {
+/*
+Print Alphabet on the console.
+
+OUTPUT  : a b c .. .. .. .. y z
+ */
+public class R07_ENG_For_while_6 {
+    public static void main(String[] args) {
+        for (int i = 'a'; i <= 'z'; i++) System.out.print((char) i + " ");
+    }
 }
