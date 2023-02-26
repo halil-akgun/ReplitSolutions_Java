af4f51d6940f3d88097da2ae24b13c756b5baeb9
halil <halilakgun0@gmail.com>
2023-02-26 23:47:32 +0300

    R32_TR_SYSO_3

---
+++ src/R32_TR_SYSO_3.java
@@ -0,0 +1,22 @@
+/*
+Verilen integer sayilarin Toplamini, carpimini, cikarma islemini ve bolme islemini yapan java kodunu yaziniz.
+
+num1=100;
+num2=25;
+
+Ornek Cikti:
+125
+2500
+75
+4
+ */
+public class R32_TR_SYSO_3 {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 25;
+        System.out.println(num1 + num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 / num2);
+    }
+}
