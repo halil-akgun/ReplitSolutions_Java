103db25cde9b7f4e6a15788589e0f6d65129f6f0
halil <halilakgun0@gmail.com>
2023-02-28 16:50:05 +0300

    R34_TR_SYSO_5

---
+++ src/R33_TR_SYSO_4.java
@@ -0,0 +1,22 @@
+/*
+Dairenin alanini ve cevresini hesaplayan java kodunu yaziniz.
+
+r=7;
+Pi=3.14
+
+Ipucu:
+Dairenin Cevresi : 2*Pi*r
+Dairenin Alani : Pi*r*r
+
+Ornek Cikti:
+43.96
+153.86
+ */
+public class R33_TR_SYSO_4 {
+    public static void main(String[] args) {
+        int r = 7;
+        double pi = 3.14;
+        System.out.println(2 * pi * r);
+        System.out.println(pi * r * r);
+    }
+}
