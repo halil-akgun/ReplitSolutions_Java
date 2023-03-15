1b2bb48d7a46203a54af0962e70efe903c55894b
halil <halilakgun0@gmail.com>
2023-03-15 17:52:52 +0300

    R48_TR_If_Switch_Ternary_StringMethods_2

---
+++ src/R48_TR_If_Switch_Ternary_StringMethods_2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/*
+Kullanıcıya Adı, Soyadı ve kredi kartı numaralarını sorarak verilen formata ceviren java kodunu yaziniz.
+
+Input :
+John White
+1234234534561478
+
+Output : Name :
+J*** W****
+CCN  : **** **** **** 1478
+
+* Ilk Harfler Buyuk harf ile baslamalidir.
+ */
+public class R48_TR_If_Switch_Ternary_StringMethods_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] nameArr = scanner.nextLine().split(" ");
+        String ccn = scanner.nextLine();
+        String name = "";
+        for (String w : nameArr) {
+            name += w.substring(0, 1).toUpperCase();
+            name += "*".repeat(w.length() - 1) + " ";
+        }
+        System.out.println(name);
+        System.out.println("CCN  : **** **** **** " + ccn.substring(ccn.length() - 4));
+    }
+}
