22415e35e8b29d9bac7b4511f95bf932b728a012
halil <halilakgun0@gmail.com>
2023-04-04 12:23:58 +0300

    R60_TR_For_while_6

---
+++ src/R60_TR_For_while_6.java
@@ -0,0 +1,13 @@
+/*
+Konsolda Alfabeyi Yazdıran programi yaziniz.
+
+OUTPUT  :
+a b c .. .. .. .. y z
+ */
+public class R60_TR_For_while_6 {
+    public static void main(String[] args) {
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
